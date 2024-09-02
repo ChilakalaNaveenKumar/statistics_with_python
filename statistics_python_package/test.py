3bd9e4759e887f773edb4ffbdc3e964b7d1a2ae7
Naveen Chilakala <nchilakala.c@lifevantage.com>
2024-09-01 21:39:23 -0500

    changing numbers directory

--- statistics_python_package/test.py
+++ statistics_python_package/test.py
@@ -12,7 +12,7 @@ from statistics_gaussian_binomial_distributions import Binomial
 class TestGaussianClass(unittest.TestCase):
     def setUp(self):
         self.gaussian = Gaussian(25, 2)
-        self.gaussian.read_data_file('number.txt')
+        self.gaussian.read_data_file('statistics_python_package/number.txt')
 
     def test_initialization(self): 
         self.assertEqual(self.gaussian.mean, 25, 'incorrect mean')
@@ -50,7 +50,7 @@ class TestGaussianClass(unittest.TestCase):
 class TestBinomialClass(unittest.TestCase):
     def setUp(self):
         self.binomial = Binomial(0.4, 20)
-        self.binomial.read_data_file('numbers_binomial.txt')
+        self.binomial.read_data_file('statistics_python_package/numbers_binomial.txt')
 
     def test_initialization(self):
         self.assertEqual(self.binomial.p, 0.4, 'p value incorrect')
